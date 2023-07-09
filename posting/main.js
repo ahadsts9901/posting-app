f4a9b5d4a1cfe39ee21add109cc8cee2b9d2bd31
ahadsts9901 <ahadsts990@gmail.com>
2023-07-09 14:31:53 -0700

    128 js

--- posting/main.js
+++ posting/main.js
@@ -54,174 +54,101 @@ var db = firebase.firestore();
 function createPost(event) {
     event.preventDefault();
 
-    Swal.fire({
-        title: "Create Poll",
-        html: `
-        <column>
-        <input id="swal-poll-question" class="swal2-input input-sw" placeholder="Enter your question" required>
-        <button id="swal-add-option-btn" class="swal2-styled add-option" type="button">+ Add Option</button>
-        <div id="swal-option-container">
-          <input id="swal-option1" class="swal2-input input-sw" placeholder="Option 1" required minLength="2" maxLength="16">
-          <input id="swal-option2" class="swal2-input input-sw" placeholder="Option 2" required minLength="2" maxLength="16">
-        </div>
-        </column>
-      `,
-        confirmButtonColor: "#252525",
-        confirmButtonText: "Add Poll",
-        showCancelButton: true,
-        cancelButtonColor: "#252525",
-        focusConfirm: true,
-        preConfirm: () => {
-            const question = document.getElementById("swal-poll-question").value;
+    // get the values
+    let post = document.getElementById("post")
 
-            const optionContainer = document.getElementById("swal-option-container");
-            const optionInputs = optionContainer.getElementsByTagName("input");
 
-            const options = [];
+    // Get the current timestamp
+    const timestamp = firebase.firestore.FieldValue.serverTimestamp();
 
-            if (question.trim() === "" || optionInputs.length < 2) {
-                Swal.showValidationMessage(
-                    "Please fill in the question and at least two options"
-                );
-                return;
-            }
+    db.collection("post")
+        .add({
+            post: post.value,
+            timestamp: timestamp,
+        })
+        .then((docRef) => {
+            console.log("Document written with ID:", docRef.id);
+            Swal.fire({
+                icon: 'success',
+                title: 'Added',
+                text: 'Post Done',
+                confirmButtonColor: "#0099ff"
+            })
+            window.location.href = "./index.html"
+            renderPosts();
+        })
+        .catch((error) => {
+            console.error("Error adding document:", error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Could Not Post',
+                confirmButtonColor: "#0099ff"
+            })
+        });
 
-            for (let i = 0; i < optionInputs.length; i++) {
-                const optionValue = optionInputs[i].value.trim();
-                if (optionValue !== "") {
-                    options.push({ text: optionValue, votes: 0 });
-                }
-            }
-
-            // Get the current timestamp
-            const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-
-            db.collection("polls")
-                .add({
-                    question: question,
-                    options: options,
-                    timestamp: timestamp,
-                })
-                .then((docRef) => {
-                    console.log("Document written with ID:", docRef.id);
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Added',
-                        text: 'Poll Added Succesfully',
-                        confirmButtonColor: "#252525"
-                    })
-                    renderPolls();
-                })
-                .catch((error) => {
-                    console.error("Error adding document:", error);
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Error In Adding Poll',
-                        confirmButtonColor: "#252525"
-                    })
-                });
-        },
-        didOpen: () => {
-            const addOptionButton = Swal.getPopup().querySelector(
-                "#swal-add-option-btn"
-            );
-            addOptionButton.addEventListener("click", addOptionToSwal);
-        },
-    });
+    post.value = ""
 }
 
-function addOptionToSwal() {
-    const optionContainer = document.getElementById("swal-option-container");
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        document.getElementById("user").innerText = user.email.slice(0, -10);
+        console.log(user.email.slice(0, -10));
+    } else {
+        document.getElementById("user").innerText = "Unknown";
+        // window.location.href = "../index.html"
+        console.log("not signed in");
+    }
+});
 
-    const optionCount = optionContainer.childElementCount + 1;
-
-    const input = document.createElement("input");
-    input.type = "text";
-    input.placeholder = "Option " + optionCount;
-    input.required = true;
-    input.className += " swal2-input input-sw";
-    // input.min = "2"
-    // input.max = "16"
-    input.minLength = "2"
-    input.maxLength = "16"
-
-    optionContainer.appendChild(input);
-}
-
-function renderPolls() {
-    let container = document.getElementById("vote-container");
-    container.innerHTML = ""; // Clear the container before rendering new polls
-
-    db.collection("polls")
+function renderPosts() {
+    let container = document.getElementById("postContainer");
+    // console.log(container.innerHTML)
+    container.innerHTML = `
+        <a href="./create.html">
+            <h2 class="add"><i style="{ fontSize: '0.8em!important';}" class="bi bi-pencil-fill"></i> Create a post</h2>
+        </a>`; // Clear the container before rendering new posts
+    db.collection("post")
         .orderBy("timestamp", "desc")
         .get()
         .then((querySnapshot) => {
             if (querySnapshot.empty) {
-                container.innerText = "No Polls Found";
+                container.innerText = "No Post Found";
             } else {
                 querySnapshot.forEach(function(doc) {
-
-                    // random colors
                     var data = doc.data();
-                    let voteCont = document.createElement("div");
-                    voteCont.className = "column border";
-                    voteCont.dataset.pollId = doc.id; // Set the poll ID in the dataset
+                    let post = document.createElement("div");
+                    post.className += " column renderPost";
 
-                    let voteHead = document.createElement("h3");
-                    voteHead.innerText = data.question;
-                    voteHead.className += " scroll"
-                    voteCont.appendChild(voteHead);
+                    let row = document.createElement("div");
+                    row.className += " row";
+                    post.appendChild(row);
 
-                    let optCont = document.createElement("div");
-                    optCont.className = "column opt-cont";
+                    let drop = document.createElement("div");
+                    drop.innerHTML = `
+                      <div class="dropdown">
+                        <button class="drop-down" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          <h2 class="bi bi-three-dots-vertical dots"></h2>
+                        </button>
+                        <ul class="dropdown-menu">
+                          <li class="list dropdown-options" type="buttons" onclick=""><i class="bi bi-trash-fill"></i> Delete Post</li>
+                          <li class="list dropdown-options" type="buttons" onclick=""><i class="bi bi-pencil-fill"></i> Edit Post</li>
+                        </ul>
+                      </div>`;
+                    row.appendChild(drop);
 
-                    if (data.options && data.options.length > 0) {
-                        let totalVotes = 0;
+                    firebase.auth().onAuthStateChanged(function(user) {
+                        if (user) {
+                            let nameText = user.email.slice(0, -10);
+                            let name = document.createElement("h2");
+                            name.innerText = nameText;
+                            row.appendChild(name);
+                        }
+                    });
 
-                        // Calculate the total number of votes
-                        data.options.forEach(function(option) {
-                            totalVotes += option.votes;
-                        });
-
-                        data.options.forEach(function(option, index) {
-                            let optionElement = document.createElement("div");
-                            optionElement.className += " row";
-                            let percentage =
-                                totalVotes > 0 ? (option.votes / totalVotes) * 100 : 0;
-                            optionElement.innerHTML = `${option.text} (${percentage.toFixed(
-                2
-              )}%)`;
-                            optionElement.style.width = `${percentage.toFixed(2)}%`;
-                            if (optionElement.style.width !== "0%") {
-                                optionElement.style.width = `${percentage.toFixed(2)}%`;
-                                optionElement.style.background = "#dadada";
-                                optionElement.style.color = "#252525"
-                            } else {
-                                optionElement.style.background = "white";
-                            }
-                            optionElement.style.padding = "0.5em";
-                            optionElement.style.wordSpacing = "0.5em";
-                            optionElement.dataset.index = index; // Set custom data attribute for referencing the option
-                            optionElement.addEventListener("click", voteOnOption);
-                            optCont.appendChild(optionElement);
-                        });
-                    } else {
-                        let noOptions = document.createElement("p");
-                        noOptions.innerText = "No options available";
-                        optCont.appendChild(noOptions);
-                    }
-
-                    voteCont.appendChild(optCont);
-
-                    let del = document.createElement("p")
-                    del.innerHTML = `<i class="bi bi-trash-fill"></i>`
-                    del.className += " right"
-                    del.addEventListener('click', () => deletePoll(doc.id));
-                    voteCont.appendChild(del)
-
-                    container.appendChild(voteCont);
+                    container.appendChild(post);
                 });
+
             }
         })
         .catch((error) => {
@@ -229,37 +156,9 @@ function renderPolls() {
         });
 }
 
-function voteOnOption(event) {
-    let optionIndex = event.target.dataset.index;
-    let pollId = event.target.closest(".border").dataset.pollId;
-
-    // Increment the vote count for the selected option
-    db.collection("polls")
-        .doc(pollId)
-        .get()
-        .then((doc) => {
-            if (doc.exists) {
-                let pollData = doc.data();
-                let options = pollData.options;
-                options[optionIndex].votes++;
-                return db.collection("polls").doc(pollId).update({
-                    options: options,
-                });
-            }
-        })
-        .then(() => {
-            console.log("Vote recorded");
-            // Set a flag indicating that the user has voted on this poll
-            // setVotedFlag(pollId);
-            renderPolls();
-        })
-        .catch((error) => {
-            console.error("Error voting on option: ", error);
-        });
-}
 
 document.addEventListener("DOMContentLoaded", async function() {
-    renderPolls();
+    renderPosts();
     // await countPolls()
 });
 
@@ -277,21 +176,6 @@ function logOut() {
         .catch((error) => {
             console.log("Sign out error:", error);
         });
-}
-
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        document.getElementById("user").innerText = user.email.slice(0, -10);
-        console.log(user.email.slice(0, -10));
-    } else {
-        document.getElementById("user").innerText = "Unknown";
-        // window.location.href = "../index.html"
-        console.log("not signed in");
-    }
-});
-
-function setVotedCookie() {
-    document.cookie = "voted=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
 
 function deletePoll(pollId) {
@@ -319,7 +203,7 @@ function deletePoll(pollId) {
                             text: 'Poll has been deleted.',
                             confirmButtonColor: '#252525',
                         });
-                        renderPolls();
+                        renderPosts();
                     })
                     .catch((error) => {
                         console.error('Error deleting poll:', error);
